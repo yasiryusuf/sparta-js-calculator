08a680c97f5fc48a9929157641b58cb67556db58
Tech-A19 <Tech-A19@Tech-A19s-MacBook-Air.local>
2017-10-30 22:39:59 +0000

    calculator is working now

--- main.js
+++ main.js
@@ -1,39 +1,38 @@
-var typeOfCalculator = prompt("Enter a for Advanced Calculator, Enter b for Basic Calculator");
-var num1 = parseFloat(prompt("Please Enter your First Number: "));
+var typeOfCalculator = prompt("Enter a for an advance calculator and b for a basic calculator");
+var num1 = parseFloat(prompt("Please Enter Your First number: "));
 var answer;
 
-if (typeOfCalculator == "a") {
-  var operation = prompt("Type p for Power and s for Square Root");
+if(typeOfCalculator == "a") {
+ var operation = prompt("Type p for power and s for square root");
 } else if (typeOfCalculator == "b") {
-  var operation = prompt("Choose an Operation (+,-,*,/)");
-} else {
-  console.log("Invalid Option");
-}
+ var operation = prompt("Choose an Operation: +,-,*,/");
+} 
 
-if(operation == "+" || operation == "-" || operation == "*" || operation == "/" || operation == "s") {
- var number2 = parseFloat(prompt("Please Enter your Second number: "));
+if(operation == "+" || operation == "-" || operation == "*" || operation == "/" || operation == "p") {
+ var num2 = parseFloat(prompt("Please Enter your Second Number: "));
+ }
 
 switch (operation) {
-  case "+":
-    answer = num1 + num2;
-    break;
-    case "-":
-    answer = num1 + num2;
-    break;
-    case "*":
-    answer = num1 - num2;
-    break;
-    case "/":
-    answer = num1 / num2;
-    break;
-    case "p":
-    answer = Math.pow(num1, num2);
-    break;
-    case "s":
-    answer = Math.sqrt(num1);
-    break;
-    default:
-    break;
+ case "+":
+   answer = num1 + num2;
+   break;
+ case "-":
+   answer = num1 - num2;
+   break;
+ case "*":
+   answer = num1 * num2;
+   break;
+ case "/":
+   answer = num1 / num2;
+   break;
+ case "p":
+   answer = Math.pow(num1, num2);
+   break;
+ case "s":
+   answer = Math.sqrt(num1);
+   break;
+ default:
+   break;
 }
 
 alert(num1 + " " + operation + " " + num2 + " = " + answer);
