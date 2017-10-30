95c4c51d0a510a1b776a14a361c18d09152aec41
Tech-A19 <Tech-A19@Tech-A19s-MacBook-Air.local>
2017-10-30 21:29:51 +0000

    done a if statement for +,-,*,/

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 var typeOfCalculator = prompt("Enter a for Advanced Calculator, Enter b for Basic Calculator");
 var num1 = parseFloat(prompt("Enter your First Number: "));
+var answer;
 
-if (typeOfCalculator == a) {
-  var operation = prompt("Type p for power and s for square root");
-} else if (typeOfCalculator == b) {
-  var operation = prompt("Type operation (+,-,*,/)");
+if (typeOfCalculator == "a") {
+  var operation = prompt("Type p for Power and s for Square Root");
+} else if (typeOfCalculator == "b") {
+  var operation = prompt("Choose an Operation (+,-,*,/)");
 } else {
   console.log("Invalid Option");
+}
+
+if(operation == + || operation == - || operation == * || operation == / || operation == "p") {
+  var num2 = parseFloat(prompt("Enter the Second Number: "));
 }
 
 switch (operation) {
