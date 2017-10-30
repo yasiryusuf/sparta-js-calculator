efbbd651987211f64474edeec1feecfe167936a6
Tech-A19 <Tech-A19@Tech-A19s-MacBook-Air.local>
2017-10-30 22:52:26 +0000

    done the README and got the calculator working

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
+
+
 var typeOfCalculator = prompt("Enter a for an advance calculator and b for a basic calculator");
 var num1 = parseFloat(prompt("Please Enter Your First number: "));
 var answer;
 
 if(typeOfCalculator == "a") {
  var operation = prompt("Type p for power and s for square root");
-} else if (typeOfCalculator == "b") {
+}else if (typeOfCalculator == "b") {
  var operation = prompt("Choose an Operation: +,-,*,/");
-} 
+}
 
 if(operation == "+" || operation == "-" || operation == "*" || operation == "/" || operation == "p") {
  var num2 = parseFloat(prompt("Please Enter your Second Number: "));
